aafd1df3b3e286cf8659203fa369df0022942fed
Nate Eckerson <nate.eckerson@gmail.com>
2018-05-02 17:14:03 -0400

    Passing returns array of shortcodes

--- spec/insta_bot_spec.rb
+++ spec/insta_bot_spec.rb
@@ -1,7 +1,19 @@
-# spec/insta-bot_spec.rb
 require 'spec_helper'
 require 'insta_bot'
 
-describe Account do
+# It's OK to test your private methods...
+# Feed in Ruby objects that are similar to what Instagram would return...
+
+describe InstaBot do
+
+  context "#build_shortcode_array" do
+    it "returns array of shortcodes" do
+      bot = new_bot
+      json = get_json
+      shortcode_ary = ["BiQW0FNnaPz", "BiQWyvqh0Lz", "BiQWuwYl-yd",
+                       "BiQWnkYg_SI", "BiQWnWnj-eP", "BiQWiwylF1j"]
+      expect(bot.send :build_shortcode_array, json).to eq shortcode_ary
+    end
+  end
 
 end
